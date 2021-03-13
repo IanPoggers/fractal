9b1d677fc914d68bcc7627ba258dd5c954821a6d
James Ian Pogue <jamesipogue@protonmail.com>
2021-03-13 11:16:08 -0800

    e

--- app/Main.hs
+++ app/Main.hs
@@ -29,16 +29,16 @@ main = do
 genFractal size iters comp =
   generateImage (genPixel comp) size size :: Image Pixel8
   where
-    genPixel c x y = div 0xff iters * (iters - boundedUntil c)
+    genPixel comp x y = div 0xff iters * (iters - boundedUntil comp)
       where
         boundedUntil =
           isBounded iters $
           fromIntegral (x - div size 2) / (fromIntegral size / 3) :+
           fromIntegral (y - div size 2) / (fromIntegral size / 3)
 
-isBounded iters c e = isBounded' 1 c
+isBounded iters c comp = isBounded' 1 c
   where
     isBounded' i z
       | i == iters = i
-      | magnitude z > e = i
+      | magnitude z > comp = i
       | otherwise = isBounded' (i + 1) $ z * z * z + c
